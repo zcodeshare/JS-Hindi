8b5c2108cada3a5164b37b8422c5d3e328970ef4
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-18 11:08:57 +0000

    datatype

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,3 +5,26 @@
 console.log(6+8);
 
 console.log("Hitesh")
+
+let age=56
+let isLoggedIn=false
+let state
+
+
+//number    => 2 to the power 53
+//bigInt
+//string    => ""
+//boolean   => true/false
+//null standalone value
+//undefinded
+//symbol    =>unique
+
+
+//object
+
+
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof null)
+console.log(typeof boolean)
