52e245a4eee5d17093183583d485a586213698d4
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-18 17:18:16 +0000

    Operations on datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -19,8 +19,8 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber)
-// console.log(typeof(stringNumber))
+console.log(stringNumber)
+console.log(typeof stringNumber)
 
 
 //***********Operations*************
