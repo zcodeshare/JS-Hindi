2c49cafb244d179717eb84829795f6c96c563d67
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-18 16:34:56 +0000

    conversion of datatype

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,23 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 
 // "44"   =>
+// "44abc"  =>NaN
+// true => 1;   false
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
