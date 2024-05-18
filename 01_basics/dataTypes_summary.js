795afde827aded1aa50ae19977df4c05f25c0af2
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-18 20:41:55 +0000

    Done

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -12,24 +12,9 @@ const id =Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId)
 
-const bignumber = 1234556788976565n
-console.log(typeof bignumber)
-
 
 // Reference (Non Primitive )
 
 // Array , Object , Functions 
 
 
-const heros = [shaktiman , "naagraj"  , "doga"]
-
-let myObj = {
-    name : "jitendra",
-    age  : 21
-}
-
-// const myFunction = function(){
-//     console.log("Hello World");
-// }
-
-// console.log(typeof outsideTemp)
