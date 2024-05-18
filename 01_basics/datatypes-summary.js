d97c6a72866218086d28c145a56bb5bb5c8079e0
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-18 19:04:21 +0000

    done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//   Primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol , Symbol , BigInt
+
+const score= 100
+const scoreValue = 100.3
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const bignumber = 1234556788976565n
+console.log(typeof bignumber)
+
+
+// Reference (Non Primitive )
+
+// Array , Object , Functions 
+
+
+const heros = [shaktiman , "naagraj"  , "doga"]
+
+let myObj = {
+    name : "jitendra",
+    age  : 21
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof outsideTemp)
