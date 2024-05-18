4b8f0a98949283925c3204fd110f7cb7a464548d
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-18 17:14:39 +0000

    operations on datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -19,5 +19,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+//***********Operations*************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// 
+
+let str1= "hello"
+let str2 = " Jitendra"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1" + 5)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"5")
+
+// console.log((3+4) *(5%3));
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 1 + 1
+gameCounter++
+
+console.log(gameCounter)
+
+let gameCounter1 = 100
+++gameCounter1
+console.log(gameCounter1)
