a486cff72808ebd154797ef721af77e565239b86
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-19 17:32:34 +0000

    done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,31 @@
 const name = "jitendrra"
 const repoCount = 50
 
-console.log(name + repoCount + " Value")
+//console.log(name + repoCount + " Value")
+
+//console.log(`Hello my name is ${name}  and my count is ${repoCount}`)
+
+const gameName = new String('hiteshhc')
+//console.log(gameName[0])
+//console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString)
+
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = new "    hitessh     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const str = "   foo  ";
+console.log(str.trim()); // 'foo'
