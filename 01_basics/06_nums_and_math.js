538de7a1d237e723ab7e6dbe200f0843c235269b
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-20 17:28:32 +0000

    All done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//************Math********/
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(5.7));
+
+console.log(Math.random());
