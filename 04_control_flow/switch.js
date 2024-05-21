4447ff646625d62e0972eb18743413a72718d518
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-21 16:21:46 +0000

    Done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("February")
+    case 3:
+        console.log("March")
+    case 4:
+        console.log("April")
+    default:
+        console.log("Enter a valid month")
+        break;
+}
