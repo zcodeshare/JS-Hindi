5f90e8e7a255d07b6de9479247efa7a29cb20cca
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-21 16:10:09 +0000

    pk

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// // IF
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if(temperature === 50){
+//     console.log("less than 50");
+// }
+// else{
+
+//     console.log("temperature is greater than 50");
+// }
+
+
+// const score =200
+// if(score>100){
+//     let power = "fly"
+//     console.log(`User power : ${power}`);
+// }
+
+// console.log(`User power : ${power}`);
+
+const balance = 1000
+
+if(balance > 500)
+    console.log("test")
+    console.log("test2")
