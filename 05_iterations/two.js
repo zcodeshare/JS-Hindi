480119194d8353491fc8accf96965320b4bf6578
Jitendra Kumar <156261937+zcodeshare@users.noreply.github.com>
2024-05-21 17:36:15 +0000

    Done

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+
+let index =0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+let myArray = ['flash','bantan',"superman"]
+
+let arr = 0
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
